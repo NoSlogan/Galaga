2857b5a527f66f9cbd3e500ae13a8db92fc769b1
NoSlogan <34922042+NoSlogan@users.noreply.github.com>
2018-01-02 19:31:37 +0100

    hs
    początek highscore

--- GameManager.cpp
+++ GameManager.cpp
@@ -2,22 +2,45 @@
 
 GameManager::GameManager()
 { 
-	SMALL_RECT r; 
+	SMALL_RECT r;
 	r.Left = r.Top = 0;
 	r.Right = 81;
 	r.Bottom = 41;
 	SetConsoleWindowInfo(GetStdHandle(STD_OUTPUT_HANDLE), TRUE, &r);
 
-	gs = GS_MENU;
-	player = nullptr;
+	gs = GS_MENU; 
+	player = nullptr; 
+	std::fstream scoreFile; 
+	scoreFile.open("scores.txt", std::ios::in); 
+	if (scoreFile.good()) 
+	{
+		for (int i = 0; i < 10; ++i)
+		{
+			scoreFile >> highScore[i].first; 
+			std::getline(scoreFile, highScore[i].second); 
+		}
+	}
+	else 
+	{
+		scoreFile.close(); 
+		scoreFile.open("scores.txt", std::ios::out | std::ios::trunc); 
+		for (int i = 0; i < 10; ++i)
+		{ 
+			scoreFile << 0; 
+			scoreFile << " nobody\n";
+			highScore[i].first = 0;
+			highScore[i].second = "nobody";
+		}
+	}
+	scoreFile.close();
 }
 
 GameManager::~GameManager()
 {
-	delete player; 
+	delete player;
 	for (unsigned i = 0; i < bullets.size(); ++i)
 	{
-		delete bullets.at(i);
+		delete bullets.at(i); 
 	}
 	for (unsigned i = 0; i < enemies.size(); ++i)
 	{
@@ -32,26 +55,26 @@ void GameManager::Input()
 		if (GetKeyState(VK_SPACE) & 0x8000) 
 		{
 			Bullet* tmpptr = new Bullet(player->x, player->y); 
-			bullets.push_back(tmpptr); 
+			bullets.push_back(tmpptr);
 		}
 		if (GetKeyState(VK_LEFT) & 0x8000)
 		{
-			player->dir = DIR_LEFT; 
+			player->dir = DIR_LEFT;
 		}
 		if (GetKeyState(VK_RIGHT) & 0x8000)
 		{
-			player->dir = DIR_RIGHT;
+			player->dir = DIR_RIGHT; 
 		}
 		if (GetKeyState(VK_LCONTROL) & 0x8000) 
 		{
-			enemies.push_back(new Enemy);
+			enemies.push_back(new Enemy); 
 		}
 	}
 }
 
 void GameManager::Render()
 {
-	int choice = 0;
+	int choice = 0; 
 	unsigned tmpPlace = 11; 
 	std::string tmpName = ""; 
 	switch (gs)
@@ -59,15 +82,14 @@ void GameManager::Render()
 	case GS_MENU:
 		while (!((choice == 1) || (choice == 2) || (choice == 3))) 
 		{
-			system("cls");
+			system("cls"); 
 			std::cout << "Wcisnij 1 Nowa gra" << std::endl;
 			std::cout << "Wcisnij 2 Highscore" << std::endl;
 			std::cout << "Wcisnij 3 Exit" << std::endl;
 			std::cin >> choice;
 		}
-		if (choice == 1) 
+		if (choice == 1)
 		{
-			
 			delete player;
 			for (unsigned i = 0; i < bullets.size(); ++i)
 			{
@@ -80,13 +102,13 @@ void GameManager::Render()
 			bullets.clear();
 			enemies.clear();
 			//
-			player = new Ship;
+			player = new Ship; 
 			timeLeft = MAX_PLAY_TIME; 
-			system("cls");
+			system("cls"); 
 			gs = GS_PLAY; 
 		}
 		else if (choice == 2)
-			gs = GS_SCORE; 
+			gs = GS_SCORE;
 		else if (choice == 3)
 			gs = GS_END; 
 		break;
@@ -98,7 +120,7 @@ void GameManager::Render()
 		}
 		for (unsigned i = 0; i < bullets.size(); ++i)
 		{
-			bullets.at(i)->Render(); 
+			bullets.at(i)->Render();
 		}
 		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), {0,36}); 
 		std::cout << "Points: " << player->score << "\t" << std::fixed << " Time left: " << timeLeft / static_cast<float>(FPS); 
@@ -106,15 +128,36 @@ void GameManager::Render()
 	case GS_PLAYEND:
 		system("cls"); 
 		std::cout << "Your score is: " << player->score << " points." << std::endl;
-		std::cout << "Press enter to continue..." << std::endl; 
+		std::cout << "Press enter to continue..." << std::endl;
 		while (!(GetKeyState(VK_RETURN) & 0x8000)) {} 
-		
+		for (int i = 9; i >= 0; --i)
+		{
+			if (player->score >= highScore[i].first)
+			{
+				tmpPlace = i;
+			}
+		}
+		if (tmpPlace < 11) 
+		{
+			std::cout << "New record! " << tmpPlace + 1 << " place" << std::endl;
+			std::cout << "Enter your name: ";
+			std::cin >> tmpName;
+			for (unsigned i = 9; i > tmpPlace + 1; --i)
+			{
+				highScore[i] = highScore[i - 1]; 
+			}
+			highScore[tmpPlace].first = player->score; 
+			highScore[tmpPlace].second = " " + tmpName;
+			UpdateScoreFile(); 
+		}
+		gs = GS_SCORE; 
+		break;
 	case GS_SCORE:
 		system("cls");
 		std::cout << "Highscore List: " << std::endl;
 		for (int i = 0; i < 10; ++i)
 		{
-			std::cout << tu beda wyniki;
+			std::cout << i + 1 << ".\t" << std::left << highScore[i].first << "\t" << highScore[i].second << std::endl; 
 		}
 		std::cout << "Press space button to go back to main menu..." << std::endl;
 		while (!(GetKeyState(VK_SPACE) & 0x8000)) {} 
@@ -135,22 +178,22 @@ void GameManager::Update()
 		player->Update(); 
 		for (unsigned i = 0; i < bullets.size(); ++i)
 		{
-			bullets.at(i)->Update();
+			bullets.at(i)->Update(); 
 			if (bullets.at(i)->y < 0) 
 			{
 				delete bullets.at(i); 
-				bullets.erase(bullets.begin() + i); 
+				bullets.erase(bullets.begin() + i);
 			}
 		}
 		for (unsigned i = 0; i < enemies.size(); ++i)
 		{
 			for (unsigned j = 0; j < bullets.size(); ++j)
 			{
-				if (enemies.at(i)->x == bullets.at(j)->x && enemies.at(i)->y == bullets.at(j)->y) 
+				if (enemies.at(i)->x == bullets.at(j)->x && enemies.at(i)->y == bullets.at(j)->y)
 				{
 					player->score++; 
-					enemies.erase(enemies.begin() + i); 
-					bullets.erase(bullets.begin() + j);
+					enemies.erase(enemies.begin() + i);
+					bullets.erase(bullets.begin() + j); 
 					i = 0; 
 					j = 0; 
 				}
@@ -179,3 +222,15 @@ void GameManager::PreRender()
 	}
 }
 
+void GameManager::UpdateScoreFile()
+{
+	std::fstream scoreFile;
+	scoreFile.open("scores.txt", std::ios::out | std::ios::trunc);
+	for (int i = 0; i < 10; ++i)
+	{
+		scoreFile << highScore[i].first; 
+		scoreFile << highScore[i].second; 
+		scoreFile << "\n"; 
+	}
+	scoreFile.close(); 
+}
