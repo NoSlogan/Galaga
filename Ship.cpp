a93d835c5eccb6c60864dea1c20f6fad81385c55
NoSlogan <34922042+NoSlogan@users.noreply.github.com>
2018-01-07 14:05:30 +0100

    update
    usuniecie błędu generowania statku poza oknem

--- Ship.cpp
+++ Ship.cpp
@@ -1,12 +1,23 @@
 #include "Ship.h"
 
 Ship::Ship()
-	: x(20), y(30)
+	: x(20), y(30) 
 {
 	dir = DIR_NONE; 
 	score = 0; 
 }
 
+void Ship::Update()
+{
+	if (this != nullptr) 
+	{
+		if (dir == DIR_LEFT && x > 0)
+			--x;
+		else if (dir == DIR_RIGHT && x < 80)
+			++x;
+		dir = DIR_NONE; 
+	}
+}
 
 void Ship::Render()
 {
