4c5b636db1c10f20d210152a10c90fd4d32cd59b
NoSlogan <34922042+NoSlogan@users.noreply.github.com>
2018-01-12 18:24:48 +0100

    zmienne odp za punkty
    zmienne odp za punkty

--- Ship.h
+++ Ship.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "Bullet.h"
 
 enum DIR
@@ -5,4 +7,19 @@ enum DIR
 	DIR_NONE = 0,
 	DIR_LEFT = 1,
 	DIR_RIGHT =2
+}; 
+
+class Ship
+{
+public:
+	short score; 
+	short x, y;
+	DIR dir;
+
+	Ship();
+
+	void Update();
+	void Render();
+	void PreRender();
 };
+
